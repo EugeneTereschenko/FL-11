87b7b44f243e4ff1c57e640665b7daa95e8ca6ea
EugeneTereschenko <tereschenko.eugene@gmail.com>
2019-07-07 11:08:32 +0300

    add HW6

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let ax = +prompt('Coordinate x first point?');
+let ay = +prompt('Coordinate y first point?');
+let bx = +prompt('Coordinate x second point?');
+let by = +prompt('Coordinate y second point?');
+let cx = +prompt('Coordinate x third point?');
+let cy = +prompt('Coordinate y third point?');
+
+let mx = (ax + bx) / 2;
+let my = (ay + by) / 2;
+
+let result = mx === cx && my === cy;
+
+console.log(result);
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,13 @@
-// Your code goes here
+let a = +prompt('Triangle first length?');
+let b = +prompt('Triangle second length?');
+let c = +prompt('Triangle third length?');
+
+if (a + b < c || a + c < b || c + b < a) {
+  console.log('Triangle doesn’t exist');
+} else if (a === b && b === c && a === c) {
+  console.log('Eequivalent triangle');
+} else if (a === b && b === c || a === c) {
+  console.log('Isosceles triangle');
+} else {
+  console.log('Normal triangle');
+}
